167187597c180aad8587c6fb74c5200ad2ecff2a
app <app@apps-MacBook-Pro.local>
2025-01-18 21:00:23 +0300

    initial commit

--- server/models.py
+++ server/models.py
@@ -22,11 +22,11 @@ class Employee(db.Model):
 
 
 class Department(db.Model):
-    __tablename__ = 'department'
+    __tablename__ = 'departments'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    address = db.Column(db.String)
+    location = db.Column(db.String)
 
     def __repr__(self):
-        return f'<Department {self.id}, {self.name}, {self.address}>'
+        return f'<Department {self.id}, {self.name}, {self.location}>'
